6510d544f600a01e3088bb9738c399c56bd726e0
vini-0123 <sonicdahora@gmail.com>
2025-03-18 09:07:34 -0300

    classe  aluno

---
+++ devopsVinicius/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace devopsVinicius.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Rgm { get; set; }
+        public string? Professor { get; set; }
+        public string? Email { get; set; }
+        public DateTime Idade { get; set; }
+    }
+}
