65a4a6c4f343c14c721b2642f2543af79042084d
vini-0123 <sonicdahora@gmail.com>
2025-03-18 08:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ devopsVinicius/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace devopsVinicius.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
